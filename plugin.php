b447d35cd5f4d089f7eb7549e5b94ad3a4b3ea9c
mukesh1510 <46702927+mukesh1510@users.noreply.github.com>
2019-01-15 18:48:18 +0530

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: WP GitHub Plugin Updater Test
 Plugin URI: https://github.com/jkudish/WordPress-GitHub-Plugin-Updater
 Description: Semi-automated test for the GitHub Plugin Updater
-Version: 2.1
+Version: 3.1
 Author: Joachim Kudish
 Author URI: http://jkudish.com/
 License: GPLv2
